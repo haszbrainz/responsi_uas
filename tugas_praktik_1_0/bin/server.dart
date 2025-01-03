d3c92df4dbb247cfecea0a2c39c1aae3533f5a8d
haszbrainz <122509529+haszbrainz@users.noreply.github.com>
2025-01-03 20:08:06 +0700

    backend

--- tugas_praktik_1_0/bin/server.dart
+++
@@ -1,6 +0,0 @@
-import 'package:vania/vania.dart';
-import 'package:tugas_praktik_1_0/config/app.dart';
-
-void main() async {
-  Application().initialize(config: config);
-}
--- tugas_praktik_1_0/lib/config/auth.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tugas_praktik_1_0/app/models/user.dart';
-
-Map<String, dynamic> authConfig = {
-  'guards': {
-    'default': {
-      'provider': User(),
-    }
-  }
-};
